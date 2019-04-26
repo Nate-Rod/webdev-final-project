97744e1dda253a15a7046faf93b47ea8a4b193a6
Nathaniel Rodriguez <nate-rod@live.com>
2019-04-26 16:49:12 -0500

    Created some skeleton code and the beginning of the rock-paper-scissors select screen

--- script.js
+++ script.js
@@ -1,10 +1,33 @@
+//Global variables
+const fullScreenBlank = "<div class='fullscreen flex-col test-box'></div>"
+
 function main(){
   //Init: load player options
+  $(document).ready(function(){
+    $("#splash-screen-main").hide().fadeIn(1500);
+  });
+}
+
+function loadPlayerOptions(){
 
 }
 
-$(document).ready(function(){
-  $("#splash-screen-main").hide().fadeIn(1500);
-});
+function loadRPSChoices(){
+  $("body").prepend(fullScreenBlank);
+}
+
+function cpuPlay(){
+
+}
+
+function playerPlay(){
+
+}
+
+function twoPlayerMode(){
+  console.log("Option chosen: Two Player Mode");
+  $("#splash-screen-main").fadeOut(100);
+  loadRPSChoices();
+}
 
 main();
