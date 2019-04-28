8365ec3d6413212087d2ec395f65fd7ba41f31b3
Nathaniel Rodriguez <nate-rod@live.com>
2019-04-28 14:36:43 -0500

    Partial transition to play screen complete

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 //Global variables
-const fullScreenBlank = "<div class='fullscreen flex-col test-box'></div>"
+const fullScreenBlank = "<div id='f-scrn' class='fullscreen flex-col'></div>"
+const chooseElement = "<div id='choose-element' class='black-bg centered-content'></div>";
+const imgSRC = "images/rps/";
+const imgArray = [imgSRC + "rock_s.png",
+                  imgSRC + "ice_s.png",
+                  imgSRC + "earth_01_s.png",
+                  imgSRC + "fire_s.png",
+                  imgSRC + "grass_s.png"];
+const bgArray = ["rock-bg ", "ice-bg ", "rock-bg ", "fire-bg ", "grass-bg "];
 
 function main(){
   //Init: load player options
   $(document).ready(function(){
-    $("#splash-screen-main").hide().fadeIn(1500);
+    $("#splash-screen-main").hide().fadeIn(500);
   });
 }
 
@@ -13,7 +21,27 @@ function loadPlayerOptions(){
 }
 
 function loadRPSChoices(){
-  $("body").prepend(fullScreenBlank);
+  var choiceContainers = [];
+  $("body").prepend(chooseElement);
+  $("#choose-element").after(fullScreenBlank);
+  //$("body").prepend(fullScreenBlank);
+  for(var i = 0; i < 5; i++){
+    choiceContainers[i] = document.createElement("div");
+    choiceContainers[i].className = bgArray[i] + "five-row centered-content flex-row";
+    choiceContainers[i].id = "img-"+i;
+    if(i % 2 != 0){
+      choiceContainers[i].className+="-r";
+    }
+    var choiceImg = document.createElement("img");
+    choiceImg.src = imgArray[i];
+    $("#f-scrn").append(choiceContainers[i]); //append images as children of container
+    document.getElementById(choiceContainers[i].id).appendChild(choiceImg);
+    console.log("Choice " + (i+1) + " appended: " + imgArray[i]);
+    console.log("Container properties:\nClassname: " + choiceContainers[i].className);
+
+    //console.log("Container array element: " + choiceContainers[i].className);
+  }
+  //console.log("Container choice array: " + choiceContainers.toString());
 }
 
 function cpuPlay(){
@@ -26,6 +54,12 @@ function playerPlay(){
 
 function twoPlayerMode(){
   console.log("Option chosen: Two Player Mode");
+  $("#splash-screen-main").fadeOut(100);
+  loadRPSChoices();
+}
+
+function onePlayerMode(){
+  console.log("Option chosen: Single Player Mode");
   $("#splash-screen-main").fadeOut(100);
   loadRPSChoices();
 }
