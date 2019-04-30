8efbc9b48a02850839d0501e6143d5610a1a45f9
Nathaniel Rodriguez <nate-rod@live.com>
2019-04-29 21:21:46 -0500

    Responsive options plus added a few more shell methods

--- script.js
+++ script.js
@@ -13,6 +13,8 @@ const elementDesc = ["rock: the erosive eraser",
                      "earth: the birthing bruiser",
                      "fire: the flaming fanatical",
                      "grass: the dryadic danger"]
+var playerNum = 1;
+var twoPlayers = true;
 
 function main(){
   //Init: load player options
@@ -21,9 +23,9 @@ function main(){
   });
 }
 
-function loadPlayerOptions(){
-
-}
+// function loadPlayerOptions(){
+//
+// }
 
 function loadRPSChoices(){
   var choiceContainers = [];
@@ -31,11 +33,10 @@ function loadRPSChoices(){
   $("body").prepend(chooseElement);
   $("#choose-element").after(fullScreenBlank);
   q = document.createElement("h6");
-  r = document.createTextNode("Player XX, choose your element.");
+  r = document.createTextNode("Player " + playerNum + ", choose your element.");
   q.className = "white";
   q.appendChild(r);
   document.getElementById("choose-element").appendChild(q);
-  //$("body").prepend(fullScreenBlank);
   for(var i = 0; i < 5; i++){
     choiceContainers[i] = document.createElement("div");
     choiceContainers[i].className = bgArray[i] + "five-row centered-content flex-row";
@@ -50,24 +51,39 @@ function loadRPSChoices(){
     p.appendChild(t);
     var choiceImg = document.createElement("img");
     choiceImg.src = imgArray[i];
+    choiceImg.id = i;
     $("#f-scrn").append(choiceContainers[i]); //append images as children of container
     choiceContainers[i].appendChild(choiceImg);
     choiceContainers[i].appendChild(elementNames[i]);
     elementNames[i].appendChild(p);
-
-    //elementNames[i].appendChild()
+    choiceImg.addEventListener("click", function(){
+      playerPlay(this.id, playerNum);
+    });
     console.log("Choice " + (i+1) + " appended: " + imgArray[i]);
     console.log("Container properties:\nClassname: " + choiceContainers[i].className);
-    //console.log("Container array element: " + choiceContainers[i].className);
   }
-  //console.log("Container choice array: " + choiceContainers.toString());
 }
 
 function cpuPlay(){
 
 }
 
-function playerPlay(){
+//see https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_document_addeventlistener_param
+//for additional help w/ passing function parameters using addEventListener
+function playerPlay(option, player){
+  console.log("Player " + player + " chose option " + option + ": " + elementDesc[option]);
+}
+
+//takes the inputs from both players and determines a winner
+function compareChoices(){
+
+}
+
+//loads the win screen with the winner of the match in the center:
+//both players' choices are displayed in the left and right sides of the screen
+//a cat photo is in the center (pulled from API) along an inspirational quote
+//"Play again?" at the bottom-center to start the game over again
+function loadWinScreen(winner){
 
 }
 
@@ -81,6 +97,7 @@ function onePlayerMode(){
   console.log("Option chosen: Single Player Mode");
   $("#splash-screen-main").fadeOut(100);
   loadRPSChoices();
+  twoPlayers = false;
 }
 
 main();
